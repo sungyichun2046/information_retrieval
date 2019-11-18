830358c932e99b9d7c14eed9d3db1a49bb2b9772
yichun <sungyichun2046@gmail.com>
2019-11-19 00:16:04 +0100

    create stop_sentences and remove them in cases

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ sys.path.append('/home/yichun/projects/information_retrieval')
 import os.path
 from process import store_decomposed_sentences_df
 
-if os.path.isfile('data/judgements'):
+if not os.path.isfile('information_retrieval/data/judgements'):
     store_decomposed_sentences_df('information_retrieval/data/judgements.jsonl')
 else:
     judgements = pd.read_csv('information_retrieval/data/judgements')
--- process.py
+++ process.py
@@ -2,7 +2,9 @@ import json
 import pandas as pd
 import re
 import string
+import collections
 punctuations = set(string.punctuation)
+punctuations.add('• ')
 
 def load_jsonl(input_path):
     """
@@ -36,13 +38,34 @@ def get_processed_texts(texts):
     texts = re.split(';|\n\n|,',texts)
     return [''.join(ch for ch in x if ch not in punctuations).strip() for x in texts if x]
 
+def remove_stop_sentence(text, stop_sentences):
+    """
+    Remove sentence if the sentence's occurrence frequency if high
+    :param text: original text
+    :param stop_sentences: stop sentences
+    :return: sentences processed
+    """
+    text_tokenized = [t.lower() for t in text if t.lower() not in stop_sentences]
+    if len(text_tokenized) == 0:
+        return text
+    else:
+        new_text = ' '.join(text_tokenized)
+    return new_text
+
 def store_decomposed_sentences_df(infile):
     df = get_dataframe(infile)
-    df['judgementId'] = df.index
-    df['text'] = df['text'].apply(lambda x: get_processed_texts(x))
-    judgements = df.set_index(['judgementId'])['text'].apply(pd.Series).stack()
-    judgements.columns = ['judgementId','lineId', 'text']
-    # df.sort_values(by='diff_top1_top2', ascending=False)
-    judgements.to_csv('data/judgements', header=True)
+    #df = df[:1000]
+    df['judgementId'] = df.index + 1
+    df['text_tokenized'] = df['text'].apply(lambda x: get_processed_texts(x))
+    stop_sentences = get_stop_sentences(df['text_tokenized'])
+    df['text'] = df['text_tokenized'].apply(lambda x: remove_stop_sentence(x, stop_sentences))
+    output_df = df[['judgementId', 'text']].copy()
+    output_df.to_csv('information_retrieval/data/judgements', header=True)
+
+def get_stop_sentences(texts):
+    texts = [text for texts in list(texts) for text in texts]
+    counter=collections.Counter(texts)
+    stop_sents = [sent.lower() for (sent, occ) in counter.most_common(850) if len(sent) > 2 and '€' not in sent and len(sent.split()) > 1 and len(sent.split()) < 5]
+    return stop_sents
 
 
