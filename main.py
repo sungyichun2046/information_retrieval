6f5e44e7f06202f1200c189afba3baabbba324a3
yichun <sungyichun2046@gmail.com>
2019-11-20 19:35:29 +0100

    add config file

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from process import store_decomposed_sentences_df
 from compute_similarity.sort import get_judgements_sorted
 from extractor_keywords import get_extracted_keywords
 
+CONFIG = json.load(open("information_retrieval/config.json"))
 #store_decomposed_sentences_df('information_retrieval/data/judgements.jsonl') # uncomment this line the first time
 judgements = pd.read_csv('information_retrieval/data/judgements')
 #Get judgements sorted according to their relevancy degree
-judgements_sorted = get_judgements_sorted(judgements, vectors="/home/yichun/projects/information_retrieval/word_embedding/fasttext_model_50d.bin", method="cosine")
+judgements_sorted = get_judgements_sorted(judgements, vectors=CONFIG['vectors_file'], method=CONFIG['sentence_similarity_method'])
 #df.iloc[:, 1] = df.iloc[:, 1].apply(lambda x: [round(float(e), 2) for e in x.split(' ')])
 judgements_sorted['keywords'] = judgements_sorted.apply(lambda row: get_extracted_keywords(row['text']), axis=1)
+
+
