c0f286f89f90c229e8c5fe7de166f54c04adabb5
yichun <sungyichun2046@gmail.com>
2019-11-20 19:18:36 +0100

    sort table and exact keywords

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import
 import sys
 import pandas as pd
+import json
 sys.path.append('/home/yichun/projects/information_retrieval')
-import os.path
 from process import store_decomposed_sentences_df
+from compute_similarity.sort import get_judgements_sorted
+from extractor_keywords import get_extracted_keywords
 
-if not os.path.isfile('information_retrieval/data/judgements'):
-    store_decomposed_sentences_df('information_retrieval/data/judgements.jsonl')
-else:
-    judgements = pd.read_csv('information_retrieval/data/judgements')
-    print(judgements.shape)
-
-
+#store_decomposed_sentences_df('information_retrieval/data/judgements.jsonl') # uncomment this line the first time
+judgements = pd.read_csv('information_retrieval/data/judgements')
+#Get judgements sorted according to their relevancy degree
+judgements_sorted = get_judgements_sorted(judgements, vectors="/home/yichun/projects/information_retrieval/word_embedding/fasttext_model_50d.bin", method="cosine")
+#df.iloc[:, 1] = df.iloc[:, 1].apply(lambda x: [round(float(e), 2) for e in x.split(' ')])
+judgements_sorted['keywords'] = judgements_sorted.apply(lambda row: get_extracted_keywords(row['text']), axis=1)
